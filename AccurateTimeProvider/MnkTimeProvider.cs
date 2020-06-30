27146b9b47763d85f732b25c8d29de15436603e3
fatkullinalina <alina2469@gmail.com>
2020-06-30 18:00:42 +0500

    30.06.2020

--- AccurateTimeProvider/MnkTimeProvider.cs
+++ AccurateTimeProvider/MnkTimeProvider.cs
@@ -2,59 +2,81 @@
 using System.Collections.Generic;
 using TimeProviderApi;
 using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AccurateTimeProviderLib
 {
-
-    public class MnkTimeProvider : ITimeProvider
+    //TODO: Старайся группировать переменные и поля в классе. Общепринятая конвенция - сначала константы, потом неизменяемые поля, потом изменяемые поля и потом свойства. Тут я уже сделал.
+    public class MnkTimeProvider : ITimeProvider, IDisposable
     {
+        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
         private readonly Queue<(long x, long y)> _timeData = new Queue<(long x, long y)>();
-        private readonly List<NtpClient> _servers = new List<NtpClient>();
+        private readonly NtpClient[] _clients;
         private readonly Stopwatch _stopwatch = new Stopwatch();
+        private readonly TimeSpan _interval;
+        private readonly int _queueLength;
+
         private Tuple<double, double> _timeCoeffs;
-        private readonly TimeSpan _interval = new TimeSpan(0, 0, 0, 0, 1000);
-        private readonly int _amount = 9;
-        public DateTime Now
+        private Task _syncTask;
+        private bool _disposed;
+
+        public DateTime Now => GetNow();
+
+        public MnkTimeProvider(string[] serverNames,int amount,TimeSpan ts)
         {
-            get
+            _interval = ts;
+            _queueLength = amount;
+            _clients = new NtpClient[serverNames.Length];
+            for(int i=0;i<serverNames.Length;i++)
             {
-                var coeffs = _timeCoeffs;
-                var timeTicks = coeffs.Item1 * _stopwatch.ElapsedTicks + coeffs.Item2;
-                var time = new DateTime((long)timeTicks, DateTimeKind.Unspecified);
-                //Console.WriteLine($"твое время: {time:dd.MM.yyyy hh:mm:ss:fffffff}");
-                return TimeZoneInfo.ConvertTimeFromUtc(time, TimeZoneInfo.Local);
+                _clients[i]=new NtpClient(serverNames[i]);
             }
-            
+            Console.WriteLine(_timeCoeffs);
         }
 
-        public MnkTimeProvider(string[] serverNames)
+        private DateTime GetNow()
         {
-            foreach(string serverName in serverNames)
-            {
-                _servers.Add(new NtpClient(serverName));
-            }
-        }
-    
-        public Task StartSync()
-        {
-            return Task.Run(SyncLoop);
+            CheckNotDisposed();
+
+            //TODO: а что будет, если запроят время, пока не было ниодной синхронизации?
+            var coeffs = _timeCoeffs;
+            var timeTicks = coeffs.Item1 * _stopwatch.ElapsedTicks + coeffs.Item2;
+            var time = new DateTime((long)timeTicks, DateTimeKind.Unspecified);
+            //Console.WriteLine($"твое время: {time:dd.MM.yyyy hh:mm:ss:fffffff}");
+            return TimeZoneInfo.ConvertTimeFromUtc(time, TimeZoneInfo.Local);
         }
 
-        private async Task SyncLoop()
+        public void StartSync()
+        {
+            if (_syncTask != null)
+            {
+                throw new InvalidOperationException("Sync already started");
+            }
+
+            CheckNotDisposed();
+            _syncTask = Task.Run(() => SyncLoop(_cancellationTokenSource.Token));
+        }
+
+        private async Task SyncLoop(CancellationToken cancellationToken)
         {
             _stopwatch.Start();
+            _timeData.Enqueue((_stopwatch.ElapsedTicks, DateTime.UtcNow.Ticks));
+            _timeData.Enqueue((_stopwatch.ElapsedTicks, DateTime.UtcNow.Ticks));
+            var res = Mnk.CountCoef(_timeData);
+            _timeCoeffs = new Tuple<double, double>(res.a, res.b);
             var i = 0;
-            while(true)
+            while (!cancellationToken.IsCancellationRequested)
             {
                 await Sync();
-                if (i > _amount)
+                if (i >= _queueLength)
                 {
                     _timeData.Dequeue();
                 }
-                var res = Mnk.CountCoef(_timeData);
+                res = Mnk.CountCoef(_timeData);
                 _timeCoeffs = new Tuple<double, double>(res.a, res.b);
-                await Task.Delay(_interval);
+
+                await Task.Delay(_interval, cancellationToken);
                 i++;
             }
         }
@@ -62,7 +84,7 @@ namespace AccurateTimeProviderLib
         private async Task Sync()
         {
             var tasks = new List<Task<DateTime>>();
-            foreach (NtpClient client in _servers)
+            foreach (NtpClient client in _clients)
             {
                 tasks.Add(Task.Run(client.RequestTime));
             }
@@ -71,6 +93,31 @@ namespace AccurateTimeProviderLib
 
             Console.WriteLine("Сервер");
             Console.WriteLine(task.Result.ToString("dd.MM.yyyy hh:mm:ss:fffffff"));
+        }
+
+        private void CheckNotDisposed()
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException(nameof(MnkTimeProvider));
+            }
+        }
+
+        //TODO: в .Net есть специальный интерфейс IDisposable. Вызов метода Dispose обзначает, что этот объект больше не нужен.
+        public void Dispose()
+        {
+            _cancellationTokenSource.Cancel();
+            _disposed = true;
+            try
+            {
+	            _syncTask?.GetAwaiter().GetResult();
+            }
+            catch (TaskCanceledException)
+            {
+                // не обрабатываем, потому что ожидаемое поведение
+            }
+
+            _cancellationTokenSource.Dispose();
         }
     }
 }
--- TimeProviderTest/Program.cs
+++ TimeProviderTest/Program.cs
@@ -1,40 +1,38 @@
 ﻿using System;
 using System.Threading.Tasks;
 using AccurateTimeProviderLib;
+using TimeProviderApi;
 
 namespace TimeProviderTest
 {
     class Program
     {
-
-        static void Main(string[] args)
-
+        static async Task Main(string[] args)
         {
             Console.WriteLine("старт");
             string[] names = { "1.ru.pool.ntp.org", "0.ru.pool.ntp.org", "2.ru.pool.ntp.org" };
+            int amount = 10;
+            TimeSpan ts = TimeSpan.FromSeconds(1);
             //StopwatchTimeProvider clock = new StopwatchTimeProvider(names);
             //LinExtrTimeProvider clock = new LinExtrTimeProvider(names);
-            ExpSglazhTimeProvider clock = new ExpSglazhTimeProvider(names);
-            //MnkTimeProvider clock = new MnkTimeProvider(names);
-            var task = clock.StartSync();
+            // ExpSglazhTimeProvider clock = new ExpSglazhTimeProvider(names);
+            using var clock = new MnkTimeProvider(names,amount, ts);
+            clock.StartSync();
+            await test(clock);
+        }
 
-            async void test()
+        static async Task test(ITimeProvider clock)
+        {
+            Console.WriteLine(TimeZoneInfo.Local);
+            Console.WriteLine(TimeZoneInfo.Utc);
+            //await Task.Delay(3000);
+            for (int i = 0; i < 20; i++)
             {
-                Console.WriteLine(TimeZoneInfo.Local);
-                Console.WriteLine(TimeZoneInfo.Utc);
-                await Task.Delay(3000);
-                for (int i = 0; i < 20; i++)
-                {
-                    Console.WriteLine("Прошу время");
-                    await Task.Delay(500);
-                    Console.WriteLine("твое время");
-                    Console.WriteLine(clock.Now.ToString("dd.MM.yyyy HH:mm:ss:fffffff"));
-                }
+                Console.WriteLine("Прошу время");
+                await Task.Delay(500);
+                Console.WriteLine("твое время");
+                Console.WriteLine(clock.Now.ToString("dd.MM.yyyy HH:mm:ss:fffffff"));
             }
-
-            test();
-
-            task.Wait();
         }
 
     }
